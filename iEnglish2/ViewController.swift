fbed3860a0909164b03029390e50ad4dc92009cb
Mulang Su <sumulang@gmail.com>
2019-08-15 08:38:56 +0800

    added outlets

--- iEnglish2/ViewController.swift
+++ iEnglish2/ViewController.swift
@@ -1,15 +1,12 @@
-//
-//  ViewController.swift
-//  iEnglish2
-//
-//  Created by Mulang Su on 14/08/2019.
-//  Copyright © 2019 Mulang Su. All rights reserved.
-//
-
 import UIKit
+import SwiftyButton
+import SnapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var playButton: PressableButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
