136f690fd1c3fa7f91809a6a3083cbdec77c5ae9
Mulang Su <sumulang@gmail.com>
2019-08-18 09:44:08 +0800

    added initialisers

--- iEnglish2/UtteranceSettingsView.swift
+++ iEnglish2/UtteranceSettingsView.swift
@@ -7,4 +7,21 @@ class UtteranceSettingsView: UIView {
     @IBOutlet var pitchSlider: UISlider!
     @IBOutlet var volumeSlider: UISlider!
     @IBOutlet var languageSegmentedControl: UISegmentedControl!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    func commonInit() {
+        Bundle.main.loadNibNamed("UtteranceSettingsView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = bounds
+        contentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    }
 }
