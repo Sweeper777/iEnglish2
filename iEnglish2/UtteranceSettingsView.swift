d423b3c570c09c07e277904cf89792221717101b
Mulang Su <sumulang@gmail.com>
2019-08-18 09:43:59 +0800

    added contentView outlet

--- iEnglish2/UtteranceSettingsView.swift
+++ iEnglish2/UtteranceSettingsView.swift
@@ -1,6 +1,8 @@
 import UIKit
 
 class UtteranceSettingsView: UIView {
+    @IBOutlet var contentView: UIView!
+    
     @IBOutlet var rateSlider: UISlider!
     @IBOutlet var pitchSlider: UISlider!
     @IBOutlet var volumeSlider: UISlider!
