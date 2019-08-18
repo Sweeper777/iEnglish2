28fa647f564bbdd3beebf094e4951541d075ebee
Mulang Su <sumulang@gmail.com>
2019-08-18 09:38:08 +0800

    connected outlets

--- iEnglish2/UtteranceSettingsView.swift
+++ iEnglish2/UtteranceSettingsView.swift
@@ -1,5 +1,8 @@
 import UIKit
 
 class UtteranceSettingsView: UIView {
-    
+    @IBOutlet var rateSlider: UISlider!
+    @IBOutlet var pitchSlider: UISlider!
+    @IBOutlet var volumeSlider: UISlider!
+    @IBOutlet var languageSegmentedControl: UISegmentedControl!
 }
