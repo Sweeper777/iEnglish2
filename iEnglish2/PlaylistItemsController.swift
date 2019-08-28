0e03c4c5fcb76dc622213aa9f13fea986ab64154
Mulang Su <sumulang@gmail.com>
2019-08-28 11:17:12 +0800

    added PlaylistItemsController properties

--- iEnglish2/PlaylistItemsController.swift
+++ iEnglish2/PlaylistItemsController.swift
@@ -1,5 +1,16 @@
 import UIKit
+import RxSwift
+import RxCocoa
+import RxDataSources
 
 class PlaylistItemsController: UITableViewController {
+    let disposeBag = DisposeBag()
+    
+    var playlist: Playlist! {
+        didSet {
+            playlistItems.accept(playlist.items)
+        }
+    }
     var playlistObject: PlaylistObject!
+    let playlistItems = BehaviorRelay<[Utterance]>(value: [])
 }
