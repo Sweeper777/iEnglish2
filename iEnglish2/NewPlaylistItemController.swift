d552de0eeba060de7a52d6085bc5d48a5ad00075
Mulang Su <sumulang@gmail.com>
2019-08-31 09:51:01 +0800

    added the two rows

--- iEnglish2/NewPlaylistItemController.swift
+++ iEnglish2/NewPlaylistItemController.swift
@@ -5,6 +5,22 @@ import SCLAlertView
 class NewPlaylistItemController: FormViewController {
     weak var delegate: NewPlaylistItemControllerDelegate?
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        form +++ Section("句子或词语")
+        
+        <<< TextAreaRow(tagContent) {
+            row in
+        }
+        
+        form +++ Section("设置")
+        
+        <<< UtteranceSettingsRow(tagUtteranceSettings) {
+            row in
+        }
+    }
+    
 }
 
 protocol NewPlaylistItemControllerDelegate : class {
