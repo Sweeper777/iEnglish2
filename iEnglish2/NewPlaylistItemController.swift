5a28ebcdf7e9c7c649cd26d8ae853da44b463809
Mulang Su <sumulang@gmail.com>
2019-08-31 09:51:15 +0800

    added showError

--- iEnglish2/NewPlaylistItemController.swift
+++ iEnglish2/NewPlaylistItemController.swift
@@ -21,6 +21,12 @@ class NewPlaylistItemController: FormViewController {
         }
     }
     
+    
+    func showError(_ message: String) {
+        let alert = SCLAlertView(appearance: SCLAlertView.SCLAppearance(showCloseButton: false))
+        alert.addButton("好", action: {})
+        alert.showError("错误!", subTitle: message)
+    }
 }
 
 protocol NewPlaylistItemControllerDelegate : class {
