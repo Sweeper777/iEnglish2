84b69bdea748802942cebf963144c4cc5a9c1d3c
Mulang Su <sumulang@gmail.com>
2019-09-05 07:50:10 +0100

    added outlet properties

--- iEnglish2/NowPlayingController.swift
+++ iEnglish2/NowPlayingController.swift
@@ -3,4 +3,14 @@ import SnapKit
 import MediaPlayer
 
 class NowPlayingController : UIViewController {
+    @IBOutlet var blur: UIVisualEffectView!
+    @IBOutlet var playlistNameLabel: UILabel!
+    @IBOutlet var utteranceTextView: UITextView!
+    @IBOutlet var utteranceTextViewContainer: UIView!
+    @IBOutlet var controlButtonsStackView: UIStackView!
+    @IBOutlet var previousButton: UIButton!
+    @IBOutlet var playPauseButton: UIButton!
+    @IBOutlet var nextButton: UIButton!
+    @IBOutlet var volumeView: MPVolumeView!
+    
 }
