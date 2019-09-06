63fce74055505046d6eb0f82cc699eebe46f348d
Mulang Su <sumulang@gmail.com>
2019-09-06 07:07:27 +0100

    added constraints for views

--- iEnglish2/NowPlayingController.swift
+++ iEnglish2/NowPlayingController.swift
@@ -31,5 +31,33 @@ class NowPlayingController : UIViewController {
             make.width.equalToSuperview()
             make.height.equalToSuperview()
         }
+        utteranceTextView.snp.makeConstraints { (make) in
+            make.center.equalToSuperview()
+            make.height.equalToSuperview()
+            make.width.equalToSuperview()
+        }
+        
+        utteranceTextViewContainer.snp.makeConstraints { (make) in
+            make.center.equalToSuperview()
+            make.height.equalTo(utteranceTextViewContainer.snp.width)
+            make.width.equalToSuperview().multipliedBy(0.75)
+        }
+        
+        playlistNameLabel.snp.makeConstraints { (make) in
+            make.centerY.equalTo(utteranceTextViewContainer.snp.top).dividedBy(2)
+            make.left.equalTo(view.safeAreaInsets.left).offset(8)
+            make.right.equalTo(view.safeAreaInsets.right).offset(-8)
+        }
+        
+        controlButtonsStackView.snp.makeConstraints { (make) in
+            make.width.equalTo(utteranceTextViewContainer.snp.width)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(utteranceTextViewContainer.snp.bottom).offset(22)
+        }
+        
+        volumeView.snp.makeConstraints { (make) in
+            make.width.equalTo(utteranceTextViewContainer.snp.width)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(controlButtonsStackView.snp.bottom).offset(14)
     }
 }
