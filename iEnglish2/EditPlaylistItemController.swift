d1a4227a199a926ba68c861f4eeecaf1de9a5c54
Mulang Su <sumulang@gmail.com>
2019-09-12 20:15:25 +0100

    copied a lot of code from NewPlaylistController

--- iEnglish2/EditPlaylistItemController.swift
+++ iEnglish2/EditPlaylistItemController.swift
@@ -4,4 +4,33 @@ import SCLAlertView
 
 class EditPlaylistItemController: FormViewController {
 
+    var utteranceObject: UtteranceObject!
+    weak var delegate: EditPlaylistItemControllerDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        form +++ Section("句子或词语")
+            
+            <<< TextAreaRow(tagContent) {
+                row in
+        }
+        
+        form +++ Section("设置")
+            
+            <<< UtteranceSettingsRow(tagUtteranceSettings) {
+                row in
+        }
+    }
+    
+    @IBAction func cancel() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    func showError(_ message: String) {
+        let alert = SCLAlertView(appearance: SCLAlertView.SCLAppearance(showCloseButton: false))
+        alert.addButton("好", action: {})
+        alert.showError("错误!", subTitle: message)
+    }
 }
