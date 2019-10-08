fa23ee5651bf7553c5995020610ae78630223d64
Mulang Su <sumulang@gmail.com>
2019-10-08 07:16:16 +0100

    started and stopped a camera session

--- iEnglish2/CameraController.swift
+++ iEnglish2/CameraController.swift
@@ -3,4 +3,15 @@ import AACameraView
 
 class CameraController: UIViewController {
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        cameraView.stopSession()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if UIImagePickerController.isCameraDeviceAvailable(.front) || UIImagePickerController.isCameraDeviceAvailable(.rear) {
+            cameraView.startSession()
+        }
+    }
 }
