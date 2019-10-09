6953b56f7d16b90f3697115d347a19cb3c400739
Mulang Su <sumulang@gmail.com>
2019-10-09 07:18:48 +0100

    overridden draw

--- iEnglish2/CameraTrigger.swift
+++ iEnglish2/CameraTrigger.swift
@@ -9,4 +9,16 @@ class CameraTrigger: UIButton {
         }
     }
     
+    override func draw(_ rect: CGRect) {
+        (UIApplication.shared.delegate as? AppDelegate).
+        let outerCircle = UIBezierPath(ovalIn: bounds.insetBy(dx: 1.5, dy: 1.5))
+        UIColor.white.setStroke()
+        outerCircle.lineWidth = 3
+        outerCircle.stroke()
+        
+        let inset = pressed ? 5.9.f : 5.f;
+        let innerCircle = UIBezierPath(ovalIn: bounds.insetBy(dx: inset, dy: inset))
+        UIColor.white.setFill()
+        innerCircle.fill()
+    }
 }
