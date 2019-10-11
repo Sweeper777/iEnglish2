608ec5e1422ac5458135a544dc7a9ab8ca5fb421
Mulang Su <sumulang@gmail.com>
2019-10-11 08:19:17 +0100

    detected text in image

--- iEnglish2/CameraResultsController.swift
+++ iEnglish2/CameraResultsController.swift
@@ -9,4 +9,24 @@ class CameraResultsController: UITableViewController {
     var textBlocks: [VisionTextBlock]?
     var textBlocksSet = false
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if !textBlocksSet {
+            textBlocksSet = true
+            EZLoadingActivity.Settings.FailText = "失败!"
+            EZLoadingActivity.show("加载中...", disableUI: true)
+            let vision = Vision.vision()
+            let textRecognizer = vision.onDeviceTextRecognizer()
+            let vImage = VisionImage(image: image)
+            textRecognizer.process(vImage) { [weak self] (result, error) in
+                guard error == nil, let result = result else {
+                    EZLoadingActivity.hide(false, animated: true)
+                    return
+                }
+                self?.textBlocks = result.blocks
+                EZLoadingActivity.hide(true, animated: true)
+                self?.tableView.reloadData()
+            }
+        }
+    }
 }
