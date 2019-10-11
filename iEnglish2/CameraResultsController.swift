b5bfc7ba57db46400dc79d77b021a18034bd038a
Mulang Su <sumulang@gmail.com>
2019-10-11 08:18:56 +0100

    Create CameraResultsController.swift

---
+++ iEnglish2/CameraResultsController.swift
@@ -0,0 +1,12 @@
+import UIKit
+import Firebase
+import EZLoadingActivity
+
+class CameraResultsController: UITableViewController {
+    
+    var image: UIImage!
+    
+    var textBlocks: [VisionTextBlock]?
+    var textBlocksSet = false
+    
+}
